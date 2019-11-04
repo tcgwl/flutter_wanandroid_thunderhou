2840574144f0b786923c5dc67cb48546bcb23834
rainHou <tcgwl_123@126.com>
2019-11-04 17:24:03 +0800

    fix AndroidX

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,10 @@ import 'package:flutter/services.dart';
 import 'package:wanandroid/view/main_page.dart';
 
 /// 程序入口
-void main() =>
-    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]) //竖屏
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]) //竖屏
       .then((_) {
-      runApp(WanApp());
-    });
+    runApp(WanApp());
+  });
+}
